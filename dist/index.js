869504a46705a83de82423bc4a2f61f65385ce00
gehlotnikhil <111073969+gehlotnikhil@users.noreply.github.com>
2024-12-10 00:55:53 +0530

    first

--- dist/index.js
+++ dist/index.js
@@ -8,23 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const { prismaMain } = require("./test");
-const client_1 = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 dotenv.config();
 const PORT = process.env.PORT;
-const prisma = new client_1.PrismaClient();
-console.log(PORT);
+// const prisma = new PrismaClient();
+// console.log(PORT);
 app.use(express.json());
 app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    prismaMain()
-        .catch((e) => {
-        console.error(e);
-    })
-        .finally(() => __awaiter(void 0, void 0, void 0, function* () { return yield prisma.$disconnect(); }));
+    // prismaMain()
+    // .catch((e:any) => {
+    //   console.error(e)
+    // })
+    // .finally(async () => await prisma.$disconnect());
     res.send({ success: true });
 }));
 app.listen(PORT, () => {
